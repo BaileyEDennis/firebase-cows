9a56f1cc3044eb1ad6e0f8b2478ad070c95e73a8
Bailey <bailey.elise.dennis@gmail.com>
2020-09-19 14:15:14 -0500

    posted users

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -1,14 +1,17 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import userData from './userData';
+import pasture from '../../components/pasture/pasture';
 
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // person is logged in
-      console.warn(user);
+      userData.getUser(user);
       $('#auth').addClass('hide');
       $('#pasture').removeClass('hide');
       $('navbar-logout-button').removeClass('hide');
+      pasture.buildCows();
     } else {
       $('#navbar-logout-button').addClass('hide');
       $('#auth').removeClass('hide');
